029289437d4b0dbe374275596e0f0ad721d72b56
Mikko Rinne <mikko.rinne@aalto.fi>
2016-10-28 09:27:11 +0300

    Commit lubm100 to Stardog.

--- tests/lubm/stardog-test/batch-speed-with-reason.sh
+++ tests/lubm/stardog-test/batch-speed-with-reason.sh
@@ -1,6 +1,8 @@
 # Test Stardog speed with the reasoner
 # Uses default reasoner, unless configured differently elsewhere
 
+# LUBM5
+
 for i in `seq 1 4` ; do
     # Note! If execution time exceeds 1 minute, remove the sed-part!!
     stardog-admin server start > /dev/null
@@ -24,6 +26,8 @@ done
 ./speed-with-reason.sh q13 lubm5
 ./speed-with-reason.sh q14 lubm5
 
+# LUBM10
+
 for i in `seq 1 4` ; do
     # Note! If execution time exceeds 1 minute, remove the sed-part!!
     stardog-admin server start > /dev/null
@@ -46,3 +50,28 @@ done
 ./speed-with-reason.sh q12 lubm10
 ./speed-with-reason.sh q13 lubm10
 ./speed-with-reason.sh q14 lubm10
+
+# LUBM100
+
+for i in `seq 1 4` ; do
+    # Note! If execution time exceeds 1 minute, remove the sed-part!!
+    stardog-admin server start > /dev/null
+    SAMPLE=$({ time stardog query -f CSV -r lubm100 "#Empty" > /dev/null; } 2>&1 | grep real | sed 's/[^0-9+\.0-9+]//g' )
+    stardog-admin server stop > /dev/null
+    Echo "Empty-query,lubm100,reasoner,$i,$SAMPLE"
+done
+
+./speed-with-reason.sh q1 lubm100
+./speed-with-reason.sh q2 lubm100
+./speed-with-reason.sh q3 lubm100
+./speed-with-reason.sh q4 lubm100
+./speed-with-reason.sh q5 lubm100
+./speed-with-reason.sh q6 lubm100
+./speed-with-reason.sh q7 lubm100
+./speed-with-reason.sh q8 lubm100
+./speed-with-reason.sh q9 lubm100
+./speed-with-reason.sh q10 lubm100
+./speed-with-reason.sh q11 lubm100
+./speed-with-reason.sh q12 lubm100
+./speed-with-reason.sh q13 lubm100
+./speed-with-reason.sh q14 lubm100
